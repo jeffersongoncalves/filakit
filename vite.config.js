cd08fa298f39d45f4d0bd83e5ef41affb8f072f6
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-05-16 08:09:18 -0300

    refactor(build): rename public to guest in filament asset paths
    Update CSS and tailwind config paths to reflect the renaming of "public" to "guest" for better clarity.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/filament/admin/theme.css',
                 'resources/css/filament/app/theme.css',
-                'resources/css/filament/public/theme.css',
+                'resources/css/filament/guest/theme.css',
                 'resources/css/app.css',
                 'resources/js/app.js'
             ],
